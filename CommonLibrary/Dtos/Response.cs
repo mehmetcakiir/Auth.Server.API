cf97e56b13f09a231f0e12f0929eb5ce063f0c82
infom <info.mehmetcakir@gmail.com>
2022-08-29 11:26:32 +0300

    Shared düzeltme

--- CommonLibrary/Dtos/Response.cs
+++ CommonLibrary/Dtos/Response.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CommonLibrary.Dtos
@@ -9,38 +10,39 @@ namespace CommonLibrary.Dtos
     public class Response<T> where T : class
     {
         public T Data { get; set; }
+        
         public int StatusCode { get; set; }
-        public ErrorDto Error { get; set; }
-
-        // Client lara gösterilmeyecek olan properti
+        
+        ErrorDto Error { get; set; }
+       
+        //Son kullanıcıya açılmayacak
+        [JsonIgnore]
         public bool IsSuccesful { get; set; }
 
-
-        //Başarılı ve geriye data dönülen
+        //Kullanıcıya veri gösterileceği başarılı durum
         public static Response<T> Succes(T data, int statusCode)
         {
-            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccesful = true };
+            return new Response<T> { Data = data, StatusCode = statusCode , IsSuccesful = true};
         }
 
-
-        //Başarılı ve sadece status code dönen
+        //Succes methodu overload edilerek kullanıcıya veri gösterilmeyen durum inşa edilmiştir.
         public static Response<T> Succes(int statusCode)
         {
-            return new Response<T> { StatusCode = statusCode, IsSuccesful = true };
+            return new Response<T> {StatusCode = statusCode, IsSuccesful = true };
         }
 
-
-        //Birden fazla hata var ise
-        public static Response<T> Faild(ErrorDto errorDto, int statusCode)
+        //1 den fazla hata durumu
+        public static Response<T> Faild(ErrorDto error, int statusCode)
         {
-            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccesful = false };
-        }
-
-        //1 Tane hate dönüyor ise
-        public static Response<T> Faild(string errorMessage, int statusCode, bool isActive)
-        {
-            var error = new ErrorDto(errorMessage, isActive);
             return new Response<T> { Error = error, StatusCode = statusCode, IsSuccesful = false };
         }
+
+        //1 hata durumu
+        public static Response<T> Faild(string error, int statusCode, bool ısShow)
+        {
+            var errorDto = new ErrorDto(error, ısShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode };
+        }
+
     }
 }
