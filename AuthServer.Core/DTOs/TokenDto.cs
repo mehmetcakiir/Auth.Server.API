e6c51324e9e2d0051bb76b4e1c0a55717c32b9f0
infom <info.mehmetcakir@gmail.com>
2022-08-31 00:09:14 +0300

    Token Dto larının kodlanması

---
+++ AuthServer.Core/DTOs/TokenDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class TokenDto
+    {
+        //Login gerektiren token alma işleminde kullanıcıya dönecek olan dto
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}
