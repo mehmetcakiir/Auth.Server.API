21832edfd269fdaa0dc8591b838e0608e361d742
infom <info.mehmetcakir@gmail.com>
2022-08-31 00:35:38 +0300

    ITokenService Kodlanması

---
+++ AuthServer.Core/Configiration/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configiration
+{
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+
+        //Token alacak olan clientın hangi api lere erişiminin olacağı tutulur.
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.Core.Configiration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        //Login gerektiren token alım işlemi (Kendi oluşturduğumuz UserApp modelini parametre olarak verilir. Geri dönüş değeri TokenDto dur.)
+        TokenDto CreateToken(UserApp userApp);
+
+        //Login gerektirmeyen token alım işlemi. (Burada parametre olarak login gerektirmediği için ayrı bir class tanımladığımız client olucaktır.)
+        ClientTokenDto CreateClientToken(Client client);
+    }
+}
