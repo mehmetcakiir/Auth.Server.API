24cd5679a443c0766fefbe3f57d00889567ab5ee
guler <guler@DESKTOP-M4R6VBO>
2025-01-05 21:18:21 +0300

    Errors bug-fix

--- CommonLibrary/Dtos/ErrorDto.cs
+++ CommonLibrary/Dtos/ErrorDto.cs
@@ -11,21 +11,31 @@ namespace CommonLibrary.Dtos
         public List<string> Errors { get; set; } = new List<string>();
         public bool IsShowe { get; set; }
 
-        //public ErrorDto()
-        //{
-        //    //Kullanım kolaylığı olması açısında construction da list new lenerek oluşturulur.
-        //    Errors = new List<string>();
-        //    IsShowe = false;
-        //}
+        /// <summary>
+        /// Kullanım kolaylığı olması açısında construction da list new lenerek oluşturulur.
+        /// </summary>
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+            IsShowe = false;
+        }
 
-        //Construction -- Bir tane hata gelme durumu
+        /// <summary>
+        /// Construction -- Bir tane hata gelme durumu
+        /// </summary>
+        /// <param name="error"></param>
+        /// <param name="ısShowe"></param>
         public ErrorDto(string error, bool ısShowe)
         {
             Errors.Add(error);
             IsShowe = ısShowe;
         }
 
-        //Construction -- Birden fazla hata gelme durumu
+        /// <summary>
+        /// Construction -- Birden fazla hata gelme durumu
+        /// </summary>
+        /// <param name="errors"></param>
+        /// <param name="ısShowe"></param>
         public ErrorDto(List<string> errors, bool ısShowe)
         {
             Errors=errors;
